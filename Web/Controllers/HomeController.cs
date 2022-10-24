12dd73a9c053e51c175db00412ed67fe67724f14
Nikita Hripach <nikita.hripach@gmail.com>
2022-10-24 21:58:42 +0300

    Add project files.

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Web.Models;
+
+namespace Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		public HomeController()
+		{
+			
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
